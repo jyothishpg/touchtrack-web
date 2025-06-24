0d33d81020f66b72c14725f48ed0af2caa89af68
jyothishtouchworld <jyothish@touchworldtechnology.com>
2025-06-24 11:03:53 +0530

    video-banner

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,21 @@
     });
   }
   window.addEventListener('load', aosInit);
+  function onSubmit(token) {
+    document.getElementById("Mycoontact").submit();
+  }
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function () {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
